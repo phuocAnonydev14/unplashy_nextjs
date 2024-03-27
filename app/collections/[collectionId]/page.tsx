c6804fe184b6ea26e09f4691dfa9c250e33bb72a
phuoc <phuoc.anonydev2k3@gmail.com>
2024-03-27 14:36:30 +0700

    handle responsive collection detail page

--- app/collections/[collectionId]/page.tsx
+++ app/collections/[collectionId]/page.tsx
@@ -22,7 +22,7 @@ export default function CollectionDetail() {
   }, [collectionId]);
 
 
-  return <div className={'px-56 py-10'}>
+  return <div className={'lg:px-56 md:px-14 sm:px-10 px-5 py-10'}>
     <h1 className={'text-center font-semibold mb-6'} style={{fontSize: "1.8rem"}}>{collection?.title}</h1>
     {collectionPhotos?.results?.length &&
         <PhotoGallery images={collectionPhotos?.results || []} collectionId={collectionId.toString()}/>
