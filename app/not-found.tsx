fcd0f8febe0d641bf16966cdab18e6fd2eeb73a3
phuoc <phuoc.anonydev2k3@gmail.com>
2024-03-28 10:08:22 +0700

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function NotFoundPage() {
+  return <div className={'h-[80dvh] flex justify-center items-center'}>
+    <Image width={600} height={700} src="/images/page-not-found.png" alt="page not found"/>
+  </div>
+}
