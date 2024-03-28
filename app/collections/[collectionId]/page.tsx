0f68c61bff6e1aaede4b00ff698af49b4575a2a0
phuoc <phuoc.anonydev2k3@gmail.com>
2024-03-28 10:56:23 +0700

    update loading state in collection detail

--- app/collections/[collectionId]/page.tsx
+++ app/collections/[collectionId]/page.tsx
@@ -30,7 +30,7 @@ export default function CollectionDetail() {
     })();
   }, [collectionId]);
 
-  if (!loading || !collection) return <EmptyData />;
+  if (!loading && !collection) return <EmptyData />;
   return (
     <div className={'lg:px-56 md:px-14 sm:px-10 px-5 py-10'}>
       <h1 className={'text-center font-semibold mb-6'} style={{ fontSize: '1.8rem' }}>
