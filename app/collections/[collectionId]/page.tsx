5d3ca56e0973b3a04219e71b0813e663231564c7
anonyDev <phuochh@nortstudio.vn>
2024-04-01 08:33:24 +0700

    change get collection detail to server component

--- app/collections/[collectionId]/page.tsx
+++ app/collections/[collectionId]/page.tsx
@@ -1,36 +1,24 @@
-'use client';
-import { useParams } from 'next/navigation';
-import useRequest from '@/common/hooks/useApiRequest';
-import { GetCollectionPhotoQueries, UnsplashService } from '@/common/services/unsplash';
-import { useEffect } from 'react';
-import { Basic } from 'unsplash-js/src/methods/photos/types';
+import { unsplashService } from '@/common/services/unsplash';
 import { PhotoGallery } from '@/components/PhotoGallery';
-import { Collection } from '@/common/types/Collection.type';
 import { EmptyData } from '@/components/shared-components/EmptyData';
 
-export default function CollectionDetail() {
-  const { collectionId } = useParams();
-  const [{ data: collection, loading }, doGetCollectionDetail] = useRequest<Collection, string>(
-    UnsplashService.getCollectionDetail
-  );
-  const [{ data: collectionPhotos }, doGetCollectionPhotos] = useRequest<
-    {
-      results: Basic[];
-    },
-    GetCollectionPhotoQueries
-  >(UnsplashService.getCollectionPhotos);
+interface CollectionDetailProps {
+  params: {
+    collectionId?: string | string[];
+  };
+}
 
-  useEffect(() => {
-    (async () => {
-      if (!collectionId || typeof collectionId !== 'string') return;
-      await Promise.all([
-        doGetCollectionPhotos({ collectionId }),
-        doGetCollectionDetail(collectionId)
-      ]);
-    })();
-  }, [collectionId]);
+export default async function CollectionDetail(props: CollectionDetailProps) {
+  const { collectionId } = props.params;
 
-  if (!loading && !collection) return <EmptyData />;
+  if (!collectionId || typeof collectionId !== 'string') return;
+
+  const [collectionPhotos, collection] = await Promise.all([
+    unsplashService.getCollectionPhotos({ collectionId }),
+    unsplashService.getCollectionDetail(collectionId)
+  ]);
+
+  if (!collectionPhotos && !collection) return <EmptyData />;
   return (
     <div className={'lg:px-56 md:px-14 sm:px-10 px-5 py-10'}>
       <h1 className={'text-center font-semibold mb-6'} style={{ fontSize: '1.8rem' }}>
